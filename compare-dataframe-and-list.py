e3d406dba654a2e687d4052031234429080a1c0f
jesper-mb <47184741+jesper-mb@users.noreply.github.com>
2021-05-23 13:38:26 +0200

    Added snippet for comparison of Pandas DataFrame and a list

---
+++ compare-dataframe-and-list.py
@@ -0,0 +1,20 @@
+#Compare a pandas dataframe column witn a list.
+#Requires Pandas
+
+#Creating example DataFrame
+df = pd.DataFrame([['panda', 'python'], ['shark', 'panda'], ['crocodile', 'python']], columns=['A', 'B'])
+
+#Creating example list
+animal_list = ['panda', 'crocodile']
+
+#Select only values that ARE in the list
+df_incl = df[df.isin(animal_list)]
+
+#Select only value that are NOT in the list
+df_excl = df[~df.isin(animal_list)]
+
+#Based on a specific column, select all rows that have values that ARE in the list
+df_incl = df[df['A'].isin(animal_list)]
+
+#Based on a specific column, select all rows that have values that are NOT in the list
+df_excl = df[df['A'].isin(animal_list)]
