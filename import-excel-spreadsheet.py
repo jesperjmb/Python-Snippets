228a530c39fc75ac25c59fa98036ba1ada22f8b1
Jesper Borup <47186052+jesperjmb@users.noreply.github.com>
2021-05-23 14:52:31 +0200

    Update import-excel-spreadsheet.py

--- import-excel-spreadsheet.py
+++ import-excel-spreadsheet.py
@@ -1,4 +1,4 @@
 #Standard import of a Excel spreadsheet
 #Requires Pandas
 #Change sheet_name to the name of the specific sheet you want to import
-df = pd.read_excel(r'you-directory\FoodEnvironmentAtlas.xls', sheet_name='excel-sheet-name')
+df = pd.read_excel(r'your-directory\file-name.xls', sheet_name='excel-sheet-name')
